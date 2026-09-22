60fb70be1f6212049462429f9f19f3af52c735c2
Andres <corderoa956@gmail.com>
2026-09-22 17:10:48 +0000

    Agrega validacion a query parameters para el get de todos los platillos

--- src/platillos/platillos.validation.js
+++ src/platillos/platillos.validation.js
@@ -13,7 +13,22 @@ const getAllPlatillosValidation = [
         .trim()
         .isInt({ min: 1, max: 50 })
         .toInt()
-        .withMessage("El límite debe estar entre 1 y 50")
+        .withMessage("El límite debe estar entre 1 y 50"),
+    query().custom((_, { req }) => {
+        const validQuerys = ["sort", "limit"];
+        const received = Object.keys(req.query);
+
+        if (received.length === 0){
+            throw new Error("Debe enviar al menos un parámetro de consulta")
+        }
+
+        const invalidQuerys = received.filter(key => !validQuerys.includes(key));
+
+        if (invalidQuerys.length > 0) {
+            throw new Error(`Querys no permitidos: ${invalidQuerys.join(", ")}`);
+        }
+        return true;
+    })
 ];
 
 const getPlatillosByIdValidation = [
