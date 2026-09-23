63f5a4dce6f9bb69cd68167e2e2c2a220c01fc48
mathivl06 <mathiasviquez1006@gmail.com>
2026-09-23 12:55:00 -0600

    tests unitarios y de integracion realizados correctamente
    tests unitarios y de integracion realizados. Se hizo ademas un cambio en el yml para poner puertos, con tal de poder realizar las pruebas. Faltal los de delete y update

--- src/config/app-env-config.js
+++ src/config/app-env-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 function requiredEnv(key) {
     const value = process.env[key];
     if (!value){
---
+++ tests/integration/platillos.test.js
@@ -0,0 +1,53 @@
+const request = require("supertest");
+const app = require("../../src/app");
+
+test("GET /health responde correctamente", async () => {
+
+    const response = await request(app)
+        .get("/paTOSrest/health");
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toEqual({
+        status: "OK"
+    });
+
+});
+
+test("GET /platillos/id responde correctamente", async () => {
+
+    const response = await request(app)
+        .get("/paTOSrest/platillos/2");
+
+    expect(response.status).toBe(200);
+
+    expect(response.body[0].platillo_id).toBe(2);
+
+});
+
+test("GET /platillos responde correctamente", async () => {
+
+    const response = await request(app)
+        .get("/paTOSrest/platillos");
+
+    expect(response.status).toBe(200);
+    console.log(response.body)
+    expect(response.body[0].platillo_id).toBe(1);
+
+});
+
+test("POST de un platillo responde correctamente", async () => {
+
+    const response = await request(app)
+        .post("/paTOSrest/platillos")
+        .send({
+            nombre: "Pato Casado",
+            descripcion: "Arroz, Frijoles, Maduro, Ensalada y una proteina (Bistec, Chuleta, Pollo a la plancha)",
+            precio: 4500
+        });
+
+    expect(response.status).toBe(201);
+
+    expect(response.body.nombre).toBe("Pato Casado");
+
+});
